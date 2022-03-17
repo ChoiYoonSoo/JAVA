3cf0d12baeeda401fb052629792c6811506b1bdd
ChoiYoonSoo <gews30025@naver.com>
2022-03-17 17:58:20 +0900

    [JAVA] 1주차 강의 실습 수정 문자열로 출력

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -2,8 +2,9 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-
+		String s1 = "Hello";
+		String s2 = "I'm a New Java Programmer";
+		System.out.println(s1+" "+s2);
 	}
 
 }
