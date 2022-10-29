65513d3cb58fc2aa20bab9ff4bd1d4862d2d616b
ChoiYoonSoo <gews30025@naver.com>
2022-10-29 21:42:32 +0900

    [JAVA] 괄호 문제 풀이
    괄호 짝 검사 여부

---
+++ 괄호.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class °ýÈ£ {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		Stack<Character> stack = new Stack<Character>();
+		
+		for(int i=0; i<n; i++) {
+			String str = br.readLine();
+			
+			for(int j=0; j<str.length(); j++) {
+				if(str.charAt(j)=='(')
+					stack.push(str.charAt(j));
+				else {
+					if(stack.empty()) {
+						stack.push(str.charAt(j));
+						break;
+					}
+					else {
+						stack.pop();
+					}
+				}
+			}
+			if(stack.empty()) {
+				System.out.println("YES");
+			}
+			else {
+				System.out.println("NO");
+			}
+		}
+		
+	}
+
+}
